3caa6d1c0706a89b70d38a900710e999773d9a48
vinod jaiswal <ervinodj@gmail.com>
2025-06-14 14:15:35 +0530

    Initial commit: full Bazel project

---
+++ main/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "helper.h"
+
+int main() {
+    std::cout << "Sum is: " << add(4, 5) << std::endl;
+    return 0;
+}
+
---
+++ tests/cpp_test.cpp
@@ -0,0 +1,8 @@
+#include "helper.h"
+#include <cassert>
+
+int main() {
+    assert(add(2, 3) == 5);
+    return 0;
+}
+
